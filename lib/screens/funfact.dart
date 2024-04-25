dbcee2e6b7d2b04b2bab74a9a60daafcf9e9ad24
salar <jafrisalar42@gmail.com>
2024-04-25 22:09:20 +0500

    latest commit

--- lib/screens/funfact.dart
+++ lib/screens/funfact.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:lottie/lottie.dart';
 
